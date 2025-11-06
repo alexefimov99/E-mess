84f9d50286b60859dd0b21f4c5a2dd316b692e85
alexefimov99 <alex.efimov99@yandex.ru>
2025-11-06 22:49:46 +0100

    Added more sidebars. Removed excess

--- widget.cpp
+++ widget.cpp
@@ -32,70 +32,62 @@ Widget::~Widget() {
 //     m_log->info("Received data: ", QString(data).toUtf8().constData());
 // }
 
-bool Widget::eventFilter(QObject* watched, QEvent* event) {
-    if (event->type() == QEvent::MouseMove) {
-        auto mouse_event = static_cast<QMouseEvent*>(event);
-        int x = mouse_event->pos().x();
+// bool Widget::eventFilter(QObject* watched, QEvent* event) {
+//     if (event->type() == QEvent::MouseMove) {
+//         auto mouse_event = static_cast<QMouseEvent*>(event);
+//         int x = mouse_event->pos().x();
 
-        if (x < 25) {
-            showSidebar();
-        }
-    }
+//         if (x < 25) {
+//             // showSidebar();
+//             // Utils::showSidebars(m_companion_sidebar, m_sidebar_animation);
+//         }
+//     }
 
-    return QWidget::eventFilter(watched, event);
-}
+//     return QWidget::eventFilter(watched, event);
+// }
 
 void Widget::createGeneralWidget() {
     m_common_elements = new ChatsCommon(this);
 }
 
 void Widget::createSidebar() {
-    m_sidebar = new Sidebar(this);
-    m_sidebar->move(0, 0);
+    // m_auto_hide_timer = new QTimer(this);
+    // m_auto_hide_timer->setSingleShot(true);
+    // connect(m_auto_hide_timer, &QTimer::timeout, this, &Widget::hideSidebar);
 
-    m_auto_hide_timer = new QTimer(this);
-    m_auto_hide_timer->setSingleShot(true);
-    connect(m_auto_hide_timer, &QTimer::timeout, this, &Widget::hideSidebar);
+    // setMouseTracking(true);
+    // installEventFilter(this);
+    QHBoxLayout* sidebars_layout = new QHBoxLayout(this);
 
-    setMouseTracking(true);
-    installEventFilter(this);
-}
+    m_contact_list_sidebar = new Sidebar(this, Sidebar::SidebarType::CONTACTS);
+    m_contact_settings_sidebar = new Sidebar(this, Sidebar::SidebarType::CONTACTS_SETTINGS);
+    m_user_settings_sidebar = new Sidebar(this, Sidebar::SidebarType::USER_SETTINGS);
+    sidebars_layout->addWidget(m_contact_list_sidebar, 0, Qt::AlignLeft);
+    sidebars_layout->addWidget(m_contact_settings_sidebar, 0, Qt::AlignLeft);
+    sidebars_layout->addStretch(0);
+    sidebars_layout->addWidget(m_user_settings_sidebar, 0, Qt::AlignRight);
+    m_main_layout->addLayout(sidebars_layout, 0, 0);
 
-void Widget::showSidebar() {
-    if (!m_sidebar->isVisible()) {
-        m_sidebar->show();
-    }
-
-    m_sidebar_animation = new QPropertyAnimation(m_sidebar, "geometry");
-    m_sidebar_animation->setDuration(300);
-    m_sidebar_animation->setStartValue(QRect(-m_sidebar->width(), 0, m_sidebar->width(), height()));
-    m_sidebar_animation->setEndValue(QRect(0, 0, m_sidebar->width(), height()));
-    m_sidebar_animation->start(QAbstractAnimation::DeleteWhenStopped);
-
-    m_auto_hide_timer->start(3000);
-}
-
-void Widget::hideSidebar() {
-    if (!m_sidebar->underMouse()) {
-        m_sidebar_animation = new QPropertyAnimation(m_sidebar, "geometry");
-        m_sidebar_animation->setDuration(300);
-        m_sidebar_animation->setStartValue(QRect(0, 0, m_sidebar->width(), height()));
-        m_sidebar_animation->setEndValue(QRect(-m_sidebar->width(), 0, m_sidebar->width(), height()));
-        m_sidebar_animation->start(QAbstractAnimation::DeleteWhenStopped);
-
-        connect(m_sidebar_animation, &QPropertyAnimation::finished, m_sidebar, &Sidebar::hide);
-    }
+    // TODO: Delete this when it will finished
+    Utils::showSidebars(this);
 }
 
 void Widget::keyPressEvent(QKeyEvent* key_event) {
     int symbol_code = key_event->key();
-    const char* symb = key_event->text().toUtf8().constData();
 
     if (key_event->modifiers() == Qt::ControlModifier) {
         if (symbol_code == Qt::Key_Q) {
             Utils::exitFromApp();
         } else if (symbol_code == Qt::Key_W) {
             Utils::closeWindow();
+        }
+    }
+
+    if (key_event->modifiers() == Qt::AltModifier) {
+        if (m_contact_list_sidebar->isVisible()) {
+            Utils::hideSidebars(this);
+        } else {
+            Utils::showSidebars(this);
         }
     }
 }
