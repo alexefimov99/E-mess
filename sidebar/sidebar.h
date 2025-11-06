eac8dd0cd007835384a89b9e0f67723a63d89193
alexefimov99 <alex.efimov99@yandex.ru>
2025-11-06 22:44:52 +0100

    Sidebars has been updated. Removed excess.

--- sidebar/sidebar.h
+++ sidebar/sidebar.h
@@ -4,16 +4,35 @@
 #include <QWidget>
 #include <QPushButton>
 
+#include "../utils/logger.h"
+
 
 class Sidebar : public QWidget {
     Q_OBJECT
 public:
-    Sidebar(QWidget* parent = nullptr);
+    enum class SidebarType {
+        CONTACTS,
+        CONTACTS_SETTINGS,
+        USER_SETTINGS
+    };
+
+public:
+    Sidebar(QWidget* parent, SidebarType sidebar_type);
     ~Sidebar();
 
+protected:
+    void mousePressEvent(QMouseEvent* event) override;
+
 private:
+    Logger* m_log;
+
     QPushButton* m_self_dialog;
     const int m_SIZE = 60;
+    const SidebarType m_sidebar_type;
+
+private:
+    void initSidebar();
+    void detailedPositionLog(QMouseEvent* event) const;
 };
 
 #endif  // SIDEBAR_H
