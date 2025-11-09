29359091a77f621539faa3b84a19bed7fad25976
alexefimov99 <alex.efimov99@yandex.ru>
2025-11-09 23:51:39 +0100

    Pinch of refactoring. Settings JSON

--- settings/settings.cpp
+++ settings/settings.cpp
@@ -1,5 +1,120 @@
 #include "settings.h"
 
-Settings::Settings() {}
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QTextStream>
 
-Settings::~Settings() {}
+#include <filesystem>
+
+#include "../utils/own_utils.h"
+
+
+Settings::Settings() : m_log(Logger::getInstance()) {
+    std::filesystem::create_directory(getSettingsPath());
+}
+
+Settings::~Settings() {
+
+}
+
+const std::filesystem::path Settings::getSettingsPath() const {
+    return Utils::getPath("settings");
+}
+
+const bool Settings::fileExist() const {
+    const auto file_path = getSettingsPath() / getFileName();
+    return std::filesystem::exists(file_path);
+}
+
+const std::filesystem::path Settings::getFilePath() const{
+    return getSettingsPath() / getFileName();
+}
+
+
+ContactSettings::ContactSettings() {
+    if (!fileExist()) {
+        setDefaults();
+    }
+}
+
+ContactSettings::~ContactSettings() {
+
+}
+
+
+UserSettings::UserSettings() {
+    if (!fileExist()) {
+        setDefaults();
+        return;
+    }
+
+    checkFields();
+}
+
+UserSettings::~UserSettings() {
+
+}
+
+void UserSettings::setDefaults() {
+    m_log->info("UserSettings: ", getFilePath());
+
+    // QJsonArray json_array;
+    QJsonObject head_object;
+    QJsonObject collect_objects;
+
+    QJsonObject window_object;
+    window_object.insert("width", 1600);
+    window_object.insert("height", 900);
+
+    QJsonObject sound_object;
+    sound_object.insert("InputVolume", 100);
+    sound_object.insert("OutputVolume", 100);
+    sound_object.insert("InputHeadphoneVolume", 100);
+    sound_object.insert("OutputHeadphoneVolume", 100);
+
+    collect_objects.insert("VersionJSON", 1);
+    collect_objects.insert("Resolution", window_object);
+    collect_objects.insert("Sound", sound_object);
+    // TODO: User should choose this one
+    collect_objects.insert("ChatsHistory", "Path to current directory");
+    collect_objects.insert("Avatar", "Path to default avatar");
+    // TODO: Memory limit for all chats. Exceeding the limit starts to remove old messages
+    collect_objects.insert("ChatMemoryLimit", 5);
+    collect_objects.insert("Theme", "default");
+    collect_objects.insert("VideoQuality", "We Will See In The Future");
+
+    head_object.insert("SelfSettings", collect_objects);
+
+    QJsonDocument doc(head_object);
+    QString json_string = doc.toJson(QJsonDocument::Indented);
+
+    QFile file(getFilePath().c_str());
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream stream(&file);
+    stream << json_string;
+    file.close();
+}
+
+void UserSettings::checkFields() {
+    QFile file(getFilePath().c_str());
+    file.open(QIODevice::ReadOnly | QIODevice::Text);
+    QString json_value = file.readAll();
+    file.close();
+
+    QJsonParseError err;
+    QJsonDocument doc = QJsonDocument::fromJson(json_value.toUtf8(), &err);
+    if (err.error != QJsonParseError::ParseError::NoError) {
+        m_log->warning("Settings: JSON parse error. ", err.errorString());
+        return;
+    }
+
+    QJsonValue version = doc.object().value("VersionJSON");
+    if (version == 1) {
+        return;
+    }
+
+    // TODO: Realize it next
+    updateJSON();
+}
--- settings/settings.h
+++ settings/settings.h
@@ -3,25 +3,71 @@
 
 #include <QWidget>
 
+#include <string_view>
+
+#include "../utils/logger.h"
+
+
 class Settings : public QWidget {
     Q_OBJECT
 public:
     Settings();
     ~Settings();
+
+    void setVolume() { }
+
+protected:
+    std::shared_ptr<Logger> m_log;
+
+protected:
+    [[nodiscard]] const std::filesystem::path getSettingsPath() const;
+    [[nodiscard]] const bool fileExist() const;
+    [[nodiscard]] const std::filesystem::path getFilePath() const;
+
+private:
+    void getVolume() { }
+
+    virtual const std::string_view getFileName() const = 0;
+
+    // TODO: Realize when video will be ready
+    virtual void setVideoQuality() { }
+    virtual void getVideoQuality() { }
 };
 
+/////////////////////////////////////////////
 class ContactSettings : public Settings {
     Q_OBJECT
 public:
     ContactSettings();
     ~ContactSettings();
+
+protected:
+    inline static constexpr std::string_view m_SETTINGS_FILE = "contact_settings.json";
+
+private:
+    bool global_settings = true;
+
+private:
+    void setDefaults() { m_log->info("ContactSettings: ", getFilePath()); };
+    const std::string_view getFileName() const override { return m_SETTINGS_FILE; };
 };
 
+/////////////////////////////////////////////
 class UserSettings : public Settings {
     Q_OBJECT
 public:
     UserSettings();
     ~UserSettings();
+
+protected:
+    inline static constexpr std::string_view m_SETTINGS_FILE = "user_settings.json";
+
+
+private:
+    const std::string_view getFileName() const override { return m_SETTINGS_FILE; };
+    void setDefaults();
+    void checkFields();
+    void updateJSON();
 };
 
 #endif  // SETTINGS_H
