a3dda6846724b0533a8c892ace09fbb62c3bb5ab
alexefimov99 <alex.efimov99@yandex.ru>
2025-11-20 21:19:07 +0100

    Added test file. I will refact it later...

--- tests/settings/settings_tests.cpp
+++ tests/settings/settings_tests.cpp
@@ -2,18 +2,107 @@
 #include <gmock/gmock.h>
 
 #include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include <QTextStream>
 
 #include <filesystem>
-#include <memory>
-#include <system_error>
 
 #include "../../settings/settings.h"
 
+// #pragma GCC diagnostic push
+// #pragma GCC diagnostic ignored "-Wunused-result"
+
+// #pragma GCC diagnostic pop
 
 static std::filesystem::path general_test_path;
 static std::filesystem::path settings_test_path;
-static QString default_json_content;
+static std::filesystem::path json_files_dir;
+
+static constexpr int VERSION = 1;
+
+class UserSettingsTests : public UserSettings {
+public:
+    UserSettingsTests(const std::filesystem::path& prefer_path)
+        : m_prefer_path(prefer_path)
+        , UserSettings(prefer_path) {
+
+    }
+
+    [[nodiscard]] const std::string_view getFileName() const override { return m_SETTINGS_FILE; };
+    QJsonObject getDefaultSettings() {
+        QJsonObject head_object;
+        QJsonObject collect_objects;
+
+        collect_objects["VersionJSON"]  = VERSION;
+        collect_objects["Resolution"]   = getWindowJsonObject();
+        collect_objects["Sound"]        = getSoundJsonObject();
+
+        head_object.insert("SelfTestSettings", collect_objects);
+        return head_object;
+    }
+
+    QJsonObject getMoreSettings() {
+        QJsonObject head_object;
+        QJsonObject collect_objects;
+
+        collect_objects["VersionJSON"]  = VERSION;
+        collect_objects["Resolution"]   = getWindowJsonObject();
+        collect_objects["Sound"]        = getSoundJsonObject();
+        collect_objects["TestField"]    = "Testing";
+
+        head_object.insert("SelfTestSettings", collect_objects);
+        return head_object;
+    }
+
+    QJsonObject getLessSettings() {
+        QJsonObject head_object;
+        QJsonObject collect_objects;
+
+        collect_objects["VersionJSON"]  = VERSION;
+        collect_objects["Sound"]        = getSoundJsonObject();
+
+        head_object.insert("SelfTestSettings", collect_objects);
+        return head_object;
+    }
+
+private:
+    QJsonObject getWindowJsonObject(const int new_values = -1) {
+        QJsonObject object;
+        if (new_values != -1) {
+            object["width"] = new_values;
+            object["height"] = new_values;
+        } else {
+            object["width"] = 1600;
+            object["height"] = 900;
+        }
+
+        return object;
+    }
+
+    QJsonObject getSoundJsonObject(const int new_values = -1) {
+        QJsonObject object;
+        if (new_values != -1) {
+            object["InputVolume"]             = new_values;
+            object["OutputVolume"]            = new_values;
+            object["InputHeadphoneVolume"]    = new_values;
+            object["OutputHeadphoneVolume"]   = new_values;
+        } else {
+            object["InputVolume"]             = 100;
+            object["OutputVolume"]            = 100;
+            object["InputHeadphoneVolume"]    = 100;
+            object["OutputHeadphoneVolume"]   = 100;
+        }
+
+        return object;
+    }
+
+private:
+    std::filesystem::path m_prefer_path;
+};
+
+///////////////////////////////////////////////////
 
 class SettingsTests : public ::testing::Test {
 public:
@@ -26,27 +115,7 @@ protected:
         settings_test_path = general_test_path / "settings";
         std::filesystem::create_directories(settings_test_path);
 
-        default_json_content = R"JSON_CONTENT({
-    "SelfSettings": {
-        "Avatar": "Path to default avatar",
-        "ChatMemoryLimit": 1048576,
-        "ChatsHistory": "Path to current directory",
-        "Resolution": {
-            "height": 900,
-            "width": 1600
-        },
-        "Sound": {
-            "InputHeadphoneVolume": 100,
-            "InputVolume": 100,
-            "OutputHeadphoneVolume": 100,
-            "OutputVolume": 100
-        },
-        "Theme": "default",
-        "VersionJSON": 1,
-        "VideoQuality": "We Will See In The Future"
-    }
-}
-)JSON_CONTENT";
+        json_files_dir = std::filesystem::path(TESTS_SOURCE_DIR) / "settings" / "json_settings";
     }
 
     static void TearDownTestSuite() {
@@ -55,58 +124,103 @@ protected:
 
     void SetUp() override {
         // contact_settings = new ContactSettings();
-        user_settings = new UserSettings(settings_test_path);
+        user_settings = new UserSettingsTests(settings_test_path);
         user_settings->settingsInit();
     }
 
     void TearDown() override {
-        for (const auto file : std::filesystem::recursive_directory_iterator(settings_test_path)) {
+        for (const auto& file : std::filesystem::recursive_directory_iterator(settings_test_path)) {
             std::filesystem::remove(file);
         }
     }
 };
 
+///////////////////////////////////////////////////
+
 TEST_F(SettingsTests, SetDefaultSettings) {
     ASSERT_TRUE(std::filesystem::exists(settings_test_path / user_settings->getFileName()));
 
-    const QString json_from_file = user_settings->readFileToString().value_or("");
-    ASSERT_EQ(default_json_content, json_from_file);
-}
+    auto* test_settings = dynamic_cast<UserSettingsTests*>(user_settings);
+    ASSERT_NE(test_settings, nullptr);
+    const QString json_from_file = test_settings->readFileToString().value_or("");
 
-TEST_F(SettingsTests, AddedNewJsonField) {
-    ASSERT_TRUE(std::filesystem::exists(settings_test_path / user_settings->getFileName()));
-    const QString json_content_with_new_field = R"JSON_CONTENT({
-    "SelfSettings": {
-        "Avatar": "Path to default avatar",
-        "ChatMemoryLimit": 1048576,
-        "ChatsHistory": "Path to current directory",
-        "NewField": "Test new field",
-        "Resolution": {
-            "height": 900,
-            "width": 1600
-        },
-        "Sound": {
-            "InputHeadphoneVolume": 100,
-            "InputVolume": 100,
-            "OutputHeadphoneVolume": 100,
-            "OutputVolume": 100
-        },
-        "Theme": "default",
-        "VersionJSON": 2,
-        "VideoQuality": "We Will See In The Future"
-    }
-}
-)JSON_CONTENT";
+    QFile file((json_files_dir / "user_settings_default.json").c_str());
+    ASSERT_TRUE(file.open(QIODevice::ReadOnly | QIODevice::Text));
 
-    QFile file((settings_test_path / user_settings->getFileName()).c_str());
-    ASSERT_TRUE(file.open(QIODevice::WriteOnly | QIODevice::Text));
-
-    QTextStream stream(&file);
-    stream << json_content_with_new_field;
+    const QString default_settings = file.readAll();
     file.close();
 
-    const QString new_settings_json = user_settings->readFileToString().value_or("");
-    ASSERT_EQ(json_content_with_new_field, new_settings_json);
+    ASSERT_EQ(default_settings, json_from_file);
 }
 
-// TODO: Create testutils
+// TEST_F(SettingsTests, AddedNewJsonField) {
+//     ASSERT_TRUE(std::filesystem::exists(settings_test_path / user_settings->getFileName()));
+//     const QString json_content_with_new_field = R"JSON_CONTENT({
+//     "SelfSettings": {
+//         "Avatar": "Path to default avatar",
+//         "ChatMemoryLimit": 1048576,
+//         "ChatsHistory": "Path to current directory",
+//         "NewField": "Test new field",
+//         "Resolution": {
+//             "height": 900,
+//             "width": 1600
+//         },
+//         "Sound": {
+//             "InputHeadphoneVolume": 100,
+//             "InputVolume": 100,
+//             "OutputHeadphoneVolume": 100,
+//             "OutputVolume": 100
+//         },
+//         "Theme": "default",
+//         "VersionJSON": 2,
+//         "VideoQuality": "We Will See In The Future"
+//     }
+// }
+// )JSON_CONTENT";
+
+//     QFile file((settings_test_path / user_settings->getFileName()).c_str());
+//     ASSERT_TRUE(file.open(QIODevice::WriteOnly | QIODevice::Text));
+
+//     QTextStream stream(&file);
+//     stream << json_content_with_new_field;
+//     file.close();
+
+//     const QString new_settings_json = user_settings->readFileToString().value_or("");
+//     ASSERT_EQ(json_content_with_new_field, new_settings_json);
+// }
+
+// // TODO: Create testutils
+
+// TEST_F(SettingsTests, RemovedJsonField) {
+//     ASSERT_TRUE(std::filesystem::exists(settings_test_path / user_settings->getFileName()));
+//     const QString json_content_without_field = R"JSON_CONTENT({
+//     "SelfSettings": {
+//         "Avatar": "Path to default avatar",
+//         "ChatMemoryLimit": 1048576,
+//         "Resolution": {
+//             "height": 900,
+//             "width": 1600
+//         },
+//         "Sound": {
+//             "InputHeadphoneVolume": 100,
+//             "InputVolume": 100,
+//             "OutputHeadphoneVolume": 100,
+//             "OutputVolume": 100
+//         },
+//         "Theme": "default",
+//         "VersionJSON": 2,
+//         "VideoQuality": "We Will See In The Future"
+//     }
+// }
+// )JSON_CONTENT";
+
+//     QFile file((settings_test_path / user_settings->getFileName()).c_str());
+//     ASSERT_TRUE(file.open(QIODevice::WriteOnly | QIODevice::Text));
+
+//     QTextStream stream(&file);
+//     stream << json_content_without_field;
+//     file.close();
+
+//     const QString new_settings_json = user_settings->readFileToString().value_or("");
+//     ASSERT_EQ(json_content_without_field, new_settings_json);
+// }
