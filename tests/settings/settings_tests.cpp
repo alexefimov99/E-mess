182f2ffc005ee16dcd13f1bf64dd5b5d93e2e2aa
alexefimov99 <alex.efimov99@yandex.ru>
2026-05-20 00:00:02 +0200

    Added some tests for settings

--- tests/settings/settings_tests.cpp
+++ tests/settings/settings_tests.cpp
@@ -1,226 +1,103 @@
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
+#include "../../src/core/settings/settings.h"
 
-#include <QFile>
-#include <QJsonArray>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QTextStream>
-
-#include <filesystem>
-
-#include "../../settings/settings.h"
-
-// #pragma GCC diagnostic push
-// #pragma GCC diagnostic ignored "-Wunused-result"
-
-// #pragma GCC diagnostic pop
-
-static std::filesystem::path general_test_path;
-static std::filesystem::path settings_test_path;
-static std::filesystem::path json_files_dir;
-
-static constexpr int VERSION = 1;
-
-class UserSettingsTests : public UserSettings {
+class SettingsTest : public ::testing::Test {
 public:
-    UserSettingsTests(const std::filesystem::path& prefer_path)
-        : m_prefer_path(prefer_path)
-        , UserSettings(prefer_path) {
-
-    }
-
-    [[nodiscard]] const std::string_view getFileName() const override { return m_SETTINGS_FILE; };
-    QJsonObject getDefaultSettings() {
-        QJsonObject head_object;
-        QJsonObject collect_objects;
-
-        collect_objects["VersionJSON"]  = VERSION;
-        collect_objects["Resolution"]   = getWindowJsonObject();
-        collect_objects["Sound"]        = getSoundJsonObject();
-
-        head_object.insert("SelfTestSettings", collect_objects);
-        return head_object;
-    }
-
-    QJsonObject getMoreSettings() {
-        QJsonObject head_object;
-        QJsonObject collect_objects;
-
-        collect_objects["VersionJSON"]  = VERSION;
-        collect_objects["Resolution"]   = getWindowJsonObject();
-        collect_objects["Sound"]        = getSoundJsonObject();
-        collect_objects["TestField"]    = "Testing";
-
-        head_object.insert("SelfTestSettings", collect_objects);
-        return head_object;
-    }
-
-    QJsonObject getLessSettings() {
-        QJsonObject head_object;
-        QJsonObject collect_objects;
-
-        collect_objects["VersionJSON"]  = VERSION;
-        collect_objects["Sound"]        = getSoundJsonObject();
-
-        head_object.insert("SelfTestSettings", collect_objects);
-        return head_object;
-    }
-
-private:
-    QJsonObject getWindowJsonObject(const int new_values = -1) {
-        QJsonObject object;
-        if (new_values != -1) {
-            object["width"] = new_values;
-            object["height"] = new_values;
-        } else {
-            object["width"] = 1600;
-            object["height"] = 900;
-        }
-
-        return object;
-    }
-
-    QJsonObject getSoundJsonObject(const int new_values = -1) {
-        QJsonObject object;
-        if (new_values != -1) {
-            object["InputVolume"]             = new_values;
-            object["OutputVolume"]            = new_values;
-            object["InputHeadphoneVolume"]    = new_values;
-            object["OutputHeadphoneVolume"]   = new_values;
-        } else {
-            object["InputVolume"]             = 100;
-            object["OutputVolume"]            = 100;
-            object["InputHeadphoneVolume"]    = 100;
-            object["OutputHeadphoneVolume"]   = 100;
-        }
-
-        return object;
-    }
-
-private:
-    std::filesystem::path m_prefer_path;
-};
-
-///////////////////////////////////////////////////
-
-class SettingsTests : public ::testing::Test {
-public:
-    // Settings* contact_settings;
-    Settings* user_settings;
+    MainSettings main_settings;
 
 protected:
-    static void SetUpTestSuite() {
-        general_test_path = std::filesystem::current_path() / "tests";
-        settings_test_path = general_test_path / "settings";
-        std::filesystem::create_directories(settings_test_path);
-
-        json_files_dir = std::filesystem::path(TESTS_SOURCE_DIR) / "settings" / "json_settings";
-    }
-
-    static void TearDownTestSuite() {
-
-    }
-
     void SetUp() override {
-        // contact_settings = new ContactSettings();
-        user_settings = new UserSettingsTests(settings_test_path);
-        user_settings->settingsInit();
+        main_settings = MainSettings();
+        main_settings.resetToDefaults();
     }
 
     void TearDown() override {
-        for (const auto& file : std::filesystem::recursive_directory_iterator(settings_test_path)) {
-            std::filesystem::remove(file);
-        }
+
     }
 };
 
-///////////////////////////////////////////////////
+TEST_F(SettingsTest, GetValue_ReturnsCorrectValue) {
+    EXPECT_EQ(main_settings.getValue("Theme").toString(), "default");
+    EXPECT_EQ(main_settings.getValue("Language").toString(), "en");
+    EXPECT_EQ(main_settings.getValue("ChatMemoryLimit").toInt(), 1 * 1024 * 1024);
 
-TEST_F(SettingsTests, SetDefaultSettings) {
-    ASSERT_TRUE(std::filesystem::exists(settings_test_path / user_settings->getFileName()));
-
-    auto* test_settings = dynamic_cast<UserSettingsTests*>(user_settings);
-    ASSERT_NE(test_settings, nullptr);
-    const QString json_from_file = test_settings->readFileToString().value_or("");
-
-    QFile file((json_files_dir / "user_settings_default.json").c_str());
-    ASSERT_TRUE(file.open(QIODevice::ReadOnly | QIODevice::Text));
-
-    const QString default_settings = file.readAll();
-    file.close();
-
-    ASSERT_EQ(default_settings, json_from_file);
+    EXPECT_EQ(main_settings.getValue("Window.Resolution.Width").toInt(), 1600);
+    EXPECT_EQ(main_settings.getValue("Window.Resolution.Height").toInt(), 900);
+    EXPECT_EQ(main_settings.getValue("Sound.Devices.InputDevice").toString(), "default");
+    EXPECT_EQ(main_settings.getValue("Sound.Volume.OutputVolume").toInt(), 100);
 }
 
-// TEST_F(SettingsTests, AddedNewJsonField) {
-//     ASSERT_TRUE(std::filesystem::exists(settings_test_path / user_settings->getFileName()));
-//     const QString json_content_with_new_field = R"JSON_CONTENT({
-//     "SelfSettings": {
-//         "Avatar": "Path to default avatar",
-//         "ChatMemoryLimit": 1048576,
-//         "ChatsHistory": "Path to current directory",
-//         "NewField": "Test new field",
-//         "Resolution": {
-//             "height": 900,
-//             "width": 1600
-//         },
-//         "Sound": {
-//             "InputHeadphoneVolume": 100,
-//             "InputVolume": 100,
-//             "OutputHeadphoneVolume": 100,
-//             "OutputVolume": 100
-//         },
-//         "Theme": "default",
-//         "VersionJSON": 2,
-//         "VideoQuality": "We Will See In The Future"
-//     }
-// }
-// )JSON_CONTENT";
+TEST_F(SettingsTest, GetValue_ReturnsDefault_WhenKeyNotFound) {
+    QJsonValue defaultVal = "not_found_default";
 
-//     QFile file((settings_test_path / user_settings->getFileName()).c_str());
-//     ASSERT_TRUE(file.open(QIODevice::WriteOnly | QIODevice::Text));
+    EXPECT_EQ(main_settings.getValue("NonExisting.Key", defaultVal), defaultVal);
+    EXPECT_EQ(main_settings.getValue("Window.NonExisting", defaultVal), defaultVal);
+}
 
-//     QTextStream stream(&file);
-//     stream << json_content_with_new_field;
-//     file.close();
 
-//     const QString new_settings_json = user_settings->readFileToString().value_or("");
-//     ASSERT_EQ(json_content_with_new_field, new_settings_json);
-// }
+TEST_F(SettingsTest, SetValue_SimpleField) {
+    main_settings.setValue("Theme", "dark");
+    EXPECT_EQ(main_settings.getValue("Theme").toString(), "dark");
+}
 
-// // TODO: Create testutils
+TEST_F(SettingsTest, SetValue_NestedField) {
+    main_settings.setValue("Window.Resolution.Width", 1920);
+    main_settings.setValue("Sound.Volume.OutputVolume", 75);
 
-// TEST_F(SettingsTests, RemovedJsonField) {
-//     ASSERT_TRUE(std::filesystem::exists(settings_test_path / user_settings->getFileName()));
-//     const QString json_content_without_field = R"JSON_CONTENT({
-//     "SelfSettings": {
-//         "Avatar": "Path to default avatar",
-//         "ChatMemoryLimit": 1048576,
-//         "Resolution": {
-//             "height": 900,
-//             "width": 1600
-//         },
-//         "Sound": {
-//             "InputHeadphoneVolume": 100,
-//             "InputVolume": 100,
-//             "OutputHeadphoneVolume": 100,
-//             "OutputVolume": 100
-//         },
-//         "Theme": "default",
-//         "VersionJSON": 2,
-//         "VideoQuality": "We Will See In The Future"
-//     }
-// }
-// )JSON_CONTENT";
+    EXPECT_EQ(main_settings.getValue("Window.Resolution.Width").toInt(), 1920);
+    EXPECT_EQ(main_settings.getValue("Sound.Volume.OutputVolume").toInt(), 75);
+}
 
-//     QFile file((settings_test_path / user_settings->getFileName()).c_str());
-//     ASSERT_TRUE(file.open(QIODevice::WriteOnly | QIODevice::Text));
+TEST_F(SettingsTest, SetValue_CreatesIntermediateObjects) {
+    main_settings.setValue("NewSection.SubSection.DeepValue", 42);
 
-//     QTextStream stream(&file);
-//     stream << json_content_without_field;
-//     file.close();
+    EXPECT_EQ(main_settings.getValue("NewSection.SubSection.DeepValue").toInt(), 42);
+    EXPECT_TRUE(main_settings.getValue("NewSection").isObject());
+    EXPECT_TRUE(main_settings.getValue("NewSection.SubSection").isObject());
+}
 
-//     const QString new_settings_json = user_settings->readFileToString().value_or("");
-//     ASSERT_EQ(json_content_without_field, new_settings_json);
-// }
+TEST_F(SettingsTest, SetValue_OverwritesExistingValue) {
+    main_settings.setValue("Window.StartMaximized", true);
+    EXPECT_TRUE(main_settings.getValue("Window.StartMaximized").toBool());
+}
+
+
+TEST_F(SettingsTest, MigrateSettings_UpdatesOldVersion) {
+    main_settings.setValue("VersionJSON", 0);
+    main_settings.setValue("Theme", "old_theme");
+    main_settings.setValue("Window.Resolution.Width", 1280);
+
+    main_settings.migrateSettings();
+
+    EXPECT_EQ(main_settings.getValue("VersionJSON").toInt(), main_settings.getCurrentVersion());
+
+    EXPECT_EQ(main_settings.getValue("Theme").toString(), "old_theme");
+    EXPECT_EQ(main_settings.getValue("Window.Resolution.Width").toInt(), 1280);
+    EXPECT_EQ(main_settings.getValue("Language").toString(), "en");
+}
+
+TEST_F(SettingsTest, MigrateSettings_AddsMissingFields) {
+    main_settings.setValue("VersionJSON", 0);
+
+    main_settings.migrateSettings();
+
+    EXPECT_EQ(main_settings.getValue("VersionJSON").toInt(), main_settings.getCurrentVersion());
+    EXPECT_FALSE(main_settings.getValue("Window").isUndefined());
+    EXPECT_FALSE(main_settings.getValue("Sound").isUndefined());
+    EXPECT_FALSE(main_settings.getValue("Theme").isUndefined());
+}
+
+TEST_F(SettingsTest, MigrateSettings_PreservesExistingNestedValues) {
+    main_settings.setValue("VersionJSON", 0);
+    main_settings.setValue("Window.Resolution.Width", 1366);
+    main_settings.setValue("Window.StartMaximized", true);
+    main_settings.setValue("Sound.Volume.OutputVolume", 50);
+
+    main_settings.migrateSettings();
+
+    EXPECT_EQ(main_settings.getValue("Window.Resolution.Width").toInt(), 1366);
+    EXPECT_TRUE(main_settings.getValue("Window.StartMaximized").toBool());
+    EXPECT_EQ(main_settings.getValue("Sound.Volume.OutputVolume").toInt(), 50);
+
+    EXPECT_EQ(main_settings.getValue("Sound.Devices.InputDevice").toString(), "default");
+}
